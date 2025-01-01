a48698e21b648f911536cc3cb4448999d3493102
Jahid458 <Jahidul898@outlook.com>
2025-01-01 17:08:26 +0600

    validate capctha

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,9 @@ import {
   } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
+import Menu from "../Pages/Menu/Menu/Menu";
+import Order from "../Pages/Order/Order/Order";
+import Login from "../Pages/Login/Login";
 
 
  export const router = createBrowserRouter([
@@ -13,6 +16,18 @@ import Home from "../Pages/Home/Home/Home";
         {
             path:'/',
             element:<Home/>
+        },
+        {
+            path:'menu',
+            element:<Menu></Menu>
+        },
+        {
+          path:'order/:category',
+          element: <Order></Order>
+        },
+        {
+          path:'login',
+          element:<Login></Login>
         }
       ]
     },
